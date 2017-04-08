0a530bd4049689e82bc74c44401a36eb5f4e2121
石泽智 <945449645@qq.com>
2017-04-08 13:48:18 +0800

    testfirst

---
+++ testfirst/src/com/first/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.first;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("这是第一个测试文件！用于做git测试！");
+
+	}
+
+}
